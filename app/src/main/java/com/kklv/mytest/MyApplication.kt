873811b246fba88c5adae7dace838dbeba3b07d6
lvzhendong <LvZhenDong@users.noreply.github.com>
2023-09-19 15:53:04 +0800

    使用DataStore存储数据

--- app/src/main/java/com/kklv/mytest/MyApplication.kt
+++ app/src/main/java/com/kklv/mytest/MyApplication.kt
@@ -20,12 +20,15 @@ class MyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        //设置下拉刷新风格
         SmartRefreshLayout.setDefaultRefreshHeaderCreator { context, layout ->
             layout.setPrimaryColorsId(R.color.white, R.color.color_666666)
             ClassicsHeader(context)
         }
+        //设置StateLayout的状态变更处理器
         StateConfig.stateChangedHandler = LestOnceAnimationStateChangedHandler()
     }
 }
 
+//用户信息持久化数据
 val Context.userInfoDataStore by preferencesDataStore(name = "userInfo")
